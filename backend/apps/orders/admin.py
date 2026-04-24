6bb3e5b5850b12ddd238c24076d6ab141f98729a
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-24 10:39:05 +0300

    register in admin

--- backend/apps/orders/admin.py
+++ backend/apps/orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Rating
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -10,3 +10,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_display  = ['id', 'customer', 'rider', 'status', 'total_amount', 'created_at']
     list_filter   = ['status']
     inlines       = [OrderItemInline]
+    
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['order', 'customer', 'rider', 'stars', 'created_at']
+    list_filter  = ['stars']    
