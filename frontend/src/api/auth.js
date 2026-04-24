5ba551c64c489b29d209469b74900fb7d11aa1f8
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-24 09:40:11 +0300

    add Notification Apis

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,3 +4,8 @@ export const register = (data)  => api.post('/users/register/', data);
 export const login    = (data)  => api.post('/users/login/', data);
 export const logout   = (data)  => api.post('/users/logout/', data);
 export const getMe    = ()      => api.get('/users/me/');
+
+export const getNotifications    = ()      => api.get('/users/notifications/');
+export const markNotificationsRead = (ids) => api.post('/users/notifications/read/', { ids });
+export const submitRating          = (data) => api.post('/orders/rate/', data);
+export const getMyRatings          = ()     => api.get('/orders/my-ratings/');
